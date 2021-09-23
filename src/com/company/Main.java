f0cf75f91f91142c86d85833a8ec9560859507ea
nikolaevfs <fedor.nik20144@gmail.com>
2021-09-23 19:09:42 +0300

    Added new sout

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello there");
-
+        System.out.println("Checing clone");
     }
 }
 
