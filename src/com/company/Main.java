1bd5ea5cc27a2a19d8ff63a7fbdac011d98ec53a
nikolaevfs <fedor.nik20144@gmail.com>
2021-09-23 18:44:27 +0300

    Added new sout

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello there");
 
-        System.out.println("Hi");
     }
 }
 
